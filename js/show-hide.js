b7387f6cd3b467fd2901af83f8fd1098a4e500da
Mannat <mannattuli13@gmail.com>
2021-11-26 12:40:14 +0530

    changin js

--- js/show-hide.js
+++ js/show-hide.js
@@ -1,13 +1,13 @@
-const projects = document.querySelectorAll(".toggle-item");
+// const projects = document.querySelectorAll(".toggle-item");
 const hide = document.querySelectorAll("#hide");
 
 function unHide() {
   if (hide[0].classList.contains("hidden")) {
-    for (let i = 0; i < projects.length; i++) {
+    for (let i = 0; i < hide.length; i++) {
       hide[i].classList.remove("hidden");
     }
   } else {
-    for (let i = 0; i < projects.length; i++) {
+    for (let i = 0; i < hide.length; i++) {
       hide[i].classList.add("hidden");
     }
   }
